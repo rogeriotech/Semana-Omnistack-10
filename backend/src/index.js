4a4e14d2c76b40b2daca6f3abd3c8665a4b2dc0c
Rogerio Rodrigues <sp.rogerio@outlook.com>
2020-01-16 00:16:33 -0300

    dotenv para isolar variaveis de ambiente

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 const express = require('express') 
 const mongoose = require('mongoose') 
 const routes = require('./routes')
+require('dotenv').config()
+
+const USER = process.env.USER
+const PASSWORD = process.env.PASSWORD
+const DB = process.env.DB
 
 const app = express() 
 app.use(express.json())
 app.use(routes)
 
-mongoose.connect('mongodb+srv://omnidev:omnipass@cluster0-erbaq.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0-erbaq.mongodb.net/${DB}?retryWrites=true&w=majority`, {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
