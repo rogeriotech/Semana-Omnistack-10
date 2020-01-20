d812b215990b0067f71c03b5bdc0e7b0021bba25
Rogerio Rodrigues <sp.rogerio@outlook.com>
2020-01-19 21:21:34 -0300

    Basic Routing Template

--- mobile/App.js
+++ mobile/App.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>And Again, here we are!!!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    color: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  title:{
-    color: '#fff', 
-    fontSize: 30
-  }
-});
